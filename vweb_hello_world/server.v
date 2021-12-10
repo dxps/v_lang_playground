d5847aa8b3bdb0c24e035d5701ea586d52ec0ae5
dxps <marius.ileana@gmail.com>
2021-12-10 18:53:22 +0200

    vweb_hello_world added

---
+++ vweb_hello_world/server.v
@@ -0,0 +1,17 @@
+module main
+
+import vweb
+
+
+struct App {
+	vweb.Context
+}
+
+fn main() {
+	vweb.run(&App{}, 8182)
+}
+
+pub fn (mut app App) index() vweb.Result {
+	return app.text('Hello world from vweb!')
+}
+
